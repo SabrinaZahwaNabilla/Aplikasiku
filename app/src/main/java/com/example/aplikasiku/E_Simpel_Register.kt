bd053f8a46c69222f71549b86a1c5641106cb3b6
nala rizqimaulida <maulidarizqinala@gmail.com>
2021-08-28 11:11:45 -0700

    Progres 1

---
+++ app/src/main/java/com/example/aplikasiku/E_Simpel_Register.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasiku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class E_Simpel_Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_esimpel_register)
+    }
+}
